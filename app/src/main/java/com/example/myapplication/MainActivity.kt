c2fa99e85111f8347ff15e907376bd1599d71ca1
이주현 <juhyun@ijuhyeon-ui-MacBookAir.local>
2022-01-06 18:00:52 +0900

    Make Main Screen

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.myapplication.databinding.ActivityCharacterInitBinding
+import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.diaryAddBtn.setOnClickListener {
+            val intent = Intent(this, CharacterInit::class.java)
+            startActivity(intent)
+        }
     }
 }
