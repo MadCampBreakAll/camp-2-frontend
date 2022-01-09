5703f1a949360d8a0363646340be02d1981b50fc
moretall <seongho4996@naver.com>
2022-01-10 02:19:07 +0900

    feat : api

--- app/src/main/java/com/example/myapplication/api/user/UserApiService.kt
+++ app/src/main/java/com/example/myapplication/api/user/UserApiService.kt
@@ -62,7 +62,82 @@ class UserApiService : BasicApiService{
         })
     }
 
+    fun updateAvatar(
+        dto: UpdateAvatarRequestDto,
+        success: (UpdateAvatarResponseDto?) -> Unit,
+        fail: ((Throwable) -> Unit)?
+    ) {
+        this.userApiProvider.updateAvatar(dto).enqueue(object: Callback<UpdateAvatarResponseDto>{
+            override fun onResponse(
+                call: Call<UpdateAvatarResponseDto>,
+                response: Response<UpdateAvatarResponseDto>
+            ) {
+                Log.d("DEBUG", "UPDATE AVATAR SUCCESS")
+                Log.d("DEBUG", response.toString())
+                Log.d("DEBUG", response.body().toString())
 
+                success(response.body());
+            }
 
+            override fun onFailure(call: Call<UpdateAvatarResponseDto>, t: Throwable) {
+                Log.d("DEBUG", "UPDATE AVATAR FAIL")
+                Log.d("DEBUG", t.toString())
+
+                fail?.invoke(t);
+            }
+        })
+    }
+
+    fun updateUI(
+        dto: UpdateUIRequestDto,
+        success: (UpdateUIResponseDto?) -> Unit,
+        fail: ((Throwable) -> Unit)?
+    ) {
+        this.userApiProvider.updateUI(dto).enqueue(object: Callback<UpdateUIResponseDto>{
+            override fun onResponse(
+                call: Call<UpdateUIResponseDto>,
+                response: Response<UpdateUIResponseDto>
+            ) {
+                Log.d("DEBUG", "UPDATE UI SUCCESS")
+                Log.d("DEBUG", response.toString())
+                Log.d("DEBUG", response.body().toString())
+
+                success(response.body());
+            }
+
+            override fun onFailure(call: Call<UpdateUIResponseDto>, t: Throwable) {
+                Log.d("DEBUG", "UPDATE UI FAIL")
+                Log.d("DEBUG", t.toString())
+
+                fail?.invoke(t);
+            }
+        })
+    }
+
+    fun checkNickname(
+        nickname: String,
+        success: (CheckNicknameResponseDto?) -> Unit,
+        fail: ((Throwable) -> Unit)?
+    ) {
+        this.userApiProvider.checkNickname(nickname).enqueue(object: Callback<CheckNicknameResponseDto>{
+            override fun onResponse(
+                call: Call<CheckNicknameResponseDto>,
+                response: Response<CheckNicknameResponseDto>
+            ) {
+                Log.d("DEBUG", "CHECK NICKNAME SUCCESS")
+                Log.d("DEBUG", response.toString())
+                Log.d("DEBUG", response.body().toString())
+
+                success(response.body())
+            }
+
+            override fun onFailure(call: Call<CheckNicknameResponseDto>, t: Throwable) {
+                Log.d("DEBUG", "CHECK NICKNAME FAIL")
+                Log.d("DEBUG", t.toString())
+
+                fail?.invoke(t)
+            }
+        })
+    }
 
 }
