0ae5dace087fc832d4466055b0720277f393e3d5
juhyuns <dhwndudkaps2@kaist.ac.kr>
2022-01-10 05:25:37 +0900

    Have to fix after get icon info in pending

--- app/src/main/java/com/example/myapplication/ui/friend/PendingFriendsAdapter.kt
+++ app/src/main/java/com/example/myapplication/ui/friend/PendingFriendsAdapter.kt
@@ -6,6 +6,7 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.api.friend.dto.PendingFriendDto
 import com.example.myapplication.databinding.PendingFriendBinding
+import com.example.myapplication.util.Character
 
 class PendingFriendsAdapter(
     private val context: Context,
@@ -42,7 +43,16 @@ class PendingFriendsAdapter(
 
     inner class ViewHolder(private val pendingFriendBinding: PendingFriendBinding) : RecyclerView.ViewHolder(pendingFriendBinding.root){
         fun bind(friend: PendingFriendDto){
-            pendingFriendBinding.name.text = friend.nickname
+
+            pendingFriendBinding.requestFriendInfo.createDiaryFriendNickName.text = friend.nickname
+//            var icon = pendingFriendBinding.requestFriendInfo.createDiaryFriendIcon
+//            val userCharacter = Character(body!!, bodyColor!!, blushColor!!, item!!)
+//            CharacterViewer(
+//                this,
+//                icon,
+//
+//            )
+
             pendingFriendBinding.accept.setOnClickListener{
                 accept(friend)
             }
