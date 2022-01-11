ca2675a3e09c8a5e14581a942502adcb1579b230
juhyuns <dhwndudkaps2@kaist.ac.kr>
2022-01-11 18:34:28 +0900

    Design image create button

---
+++ app/src/main/java/com/example/myapplication/ui/page/create/CreateImagePageActivity.kt
@@ -0,0 +1,12 @@
+package com.example.myapplication.ui.page.create
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myapplication.R
+
+class CreateImagePageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_image_page)
+    }
+}
